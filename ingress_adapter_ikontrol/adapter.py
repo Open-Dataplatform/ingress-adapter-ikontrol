783b5ed684e85ab3cab1e3d7dc9d9dcd2da84045
runethorbek <runethorbek@gmail.com>
2021-11-02 15:00:57 +0100

    us228426 - adding conf+cred flags

--- ingress_adapter_ikontrol/adapter.py
+++ ingress_adapter_ikontrol/adapter.py
@@ -1,6 +1,7 @@
 """
 iKontrol Adapter for Ingress.
 """
+import argparse
 import json
 import os
 import shutil
@@ -280,17 +281,42 @@ class IKontrolAdapter(IngressAdapter):
         pass
 
 
+def __init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description='Ingress adapter for JAO')
+
+    parser.add_argument('--conf',
+                        nargs='+',
+                        default=['conf.ini', '/etc/osiris/conf.ini'],
+                        help='setting the configuration file')
+    parser.add_argument('--credentials',
+                        nargs='+',
+                        default=['credentials.ini', '/vault/secrets/credentials.ini'],
+                        help='setting the credential file')
+
+    return parser
+
+
+# pylint: disable=too-many-locals
 def main():
     """
     Initialize class and upload ZIP file.
     """
+    arg_parser = __init_argparse()
+    args, _ = arg_parser.parse_known_args()
+
     config = ConfigParser()
-    config.read(['conf.ini', '/etc/osiris/conf.ini'])
+    config.read(args.conf)
     credentials_config = ConfigParser()
-    credentials_config.read(['credentials.ini', '/vault/secrets/credentials.ini'])
+    credentials_config.read(args.credentials)
 
     logging.config.fileConfig(fname=config['Logging']['configuration_file'],  # type: ignore
                               disable_existing_loggers=False)
+
+    # To disable azure INFO logging from Azure
+    if config.has_option('Logging', 'disable_logger_labels'):
+        disable_logger_labels = config['Logging']['disable_logger_labels'].splitlines()
+        for logger_label in disable_logger_labels:
+            logging.getLogger(logger_label).setLevel(logging.WARNING)
 
     tenant_id = credentials_config['Authorization']['tenant_id']
     client_id = credentials_config['Authorization']['client_id']
